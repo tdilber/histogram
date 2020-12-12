c71456b6db009bdec5bb2ebb3b087d185ea97d23
Talha DİLBER <talhadilber@hotmail.com>
2020-12-12 23:48:28 +0300

    - Main Class Edited

--- src/main/java/com/talha/interview/HistogramApp.java
+++ src/main/java/com/talha/interview/HistogramApp.java
@@ -2,6 +2,7 @@ package com.talha.interview;
 
 import com.talha.interview.histogram.Histogram;
 import com.talha.interview.histogram.HistogramLeftContainInterval;
+import com.talha.interview.histogram.creator.FileDoubleHistogramCreator;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +13,24 @@ import org.slf4j.LoggerFactory;
 public class HistogramApp {
     private final static Logger log = LoggerFactory.getLogger(HistogramApp.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BasicConfigurator.configure();
+
+        mailExample();
+        readWithFile();
+    }
+
+    private static void readWithFile() throws Exception {
+        FileDoubleHistogramCreator fileDoubleHistogramCreator = new FileDoubleHistogramCreator("histogram1.txt");
+        Histogram<Double> histogram = fileDoubleHistogramCreator.create();
+
+        log.info(histogram.toStringValueMap());
+        log.info(histogram.toStringOutLiners());
+        log.info("sample mean: " + histogram.mean());
+        log.info("sample variance: " + histogram.variance());
+    }
+
+    private static void mailExample() {
         Histogram<Double> histogram = new Histogram<>();
         histogram.addInterval(HistogramLeftContainInterval.of(0d, 1.1d));
         histogram.addInterval(HistogramLeftContainInterval.of(3d, 4.1d));
@@ -28,9 +45,9 @@ public class HistogramApp {
         histogram.addValue(1d);
         histogram.addValue(41.27d);
 
-        System.out.println(histogram.toStringValueMap());
-        System.out.println(histogram.toStringOutLiners());
-        System.out.println("sample mean: " + histogram.mean());
-        System.out.println("sample variance: " + histogram.variance());
+        log.info(histogram.toStringValueMap());
+        log.info(histogram.toStringOutLiners());
+        log.info("sample mean: " + histogram.mean());
+        log.info("sample variance: " + histogram.variance());
     }
 }
