d98da9450a6ca6135d17f6a4586e8cfbacc5fb5c
Talha DİLBER <talhadilber@hotmail.com>
2020-12-12 20:17:23 +0300

    - Logger Configuration added

--- src/main/java/com/talha/interview/HistogramApp.java
+++ src/main/java/com/talha/interview/HistogramApp.java
@@ -2,16 +2,22 @@ package com.talha.interview;
 
 import com.talha.interview.histogram.Histogram;
 import com.talha.interview.histogram.HistogramLeftContainInterval;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by tdilber at 12-Dec-20
  */
 public class HistogramApp {
+    private final static Logger log = LoggerFactory.getLogger(HistogramApp.class);
+
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         Histogram<Double> histogram = new Histogram<>();
+        histogram.addInterval(HistogramLeftContainInterval.of(0d, 1.1d));
         histogram.addInterval(HistogramLeftContainInterval.of(3d, 4.1d));
         histogram.addInterval(HistogramLeftContainInterval.of(8.5d, 8.7d));
-        histogram.addInterval(HistogramLeftContainInterval.of(0d, 1.1d));
         histogram.addInterval(HistogramLeftContainInterval.of(31.5d, 41.27d));
 
         histogram.addValue(40.1d);
